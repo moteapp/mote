64d28fdb7f99588a6a0e77979c659cd8d290e915
Evan Zhu <lifereduce@gmail.com>
2024-04-23 23:29:34 +0800

    Add alias for vite rollup

--- packages/mote-web/vite.config.ts
+++ packages/mote-web/vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 //import react from '@vitejs/plugin-react-swc'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths()
-  ]
+  ],
+  resolve: {
+    alias: {
+      'mote': path.resolve(__dirname, './src/mote'),
+    }
+  }
 })
