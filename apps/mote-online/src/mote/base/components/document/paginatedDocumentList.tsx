d3ff25816abc90b385b9eba7950e2333320ba896
Evan Zhu <lifereduce@gmail.com>
2024-06-30 10:40:23 +0800

    Fix build error

--- apps/mote-online/src/mote/base/components/document/paginatedDocumentList.tsx
+++ apps/mote-online/src/mote/base/components/document/paginatedDocumentList.tsx
@@ -10,7 +10,7 @@ export const PaginatedDocumentList = ({
     empty
 }:IPaginatedDocumentListProps) => {
     return (
-        <PaginatedList >
+        <PaginatedList items={[]}>
             
         </PaginatedList>
     )
