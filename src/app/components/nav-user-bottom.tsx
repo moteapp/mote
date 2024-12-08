607b295feda8d44bee9e6a5bcf7e003994e2cdbf
Evan Zhu <lifereduce@gmail.com>
2024-12-08 10:01:49 +0800

    Add avatar for user

--- src/app/components/nav-user-bottom.tsx
+++ src/app/components/nav-user-bottom.tsx
@@ -50,7 +50,7 @@ export function NavUser() {
             <div className="flex items-center gap-2 px-2 py-1.5 text-left text-sm transition-all">
             <Avatar className="h-7 w-7 rounded-md border">
                 <AvatarImage
-                src={user.avatar}
+                src={user.avatar ?? undefined}
                 alt={user.name}
                 className="animate-in fade-in-50 zoom-in-90"
                 />
@@ -74,7 +74,7 @@ export function NavUser() {
             <DropdownMenuLabel className="p-0 font-normal">
             <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm transition-all">
                 <Avatar className="h-7 w-7 rounded-md">
-                <AvatarImage src={user.avatar} alt={user.name} />
+                <AvatarImage src={user.avatar ?? undefined } alt={user.name} />
                 <AvatarFallback>CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1">
